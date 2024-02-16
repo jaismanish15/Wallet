2d439fb86591b4146006b8d022ef8e105dff08ac
Manish Jaiswal <manish.jaiswal_ftc@external.swiggy.in>
2024-02-16 12:53:51 +0530

    Initial Commit - Completed project till user api, yet to associate wallet with user

--- src/main/java/swiggy/wallet/controller/RegistrationController.java
+++ src/main/java/swiggy/wallet/controller/RegistrationController.java
@@ -1,6 +1,7 @@
 package swiggy.wallet.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,8 +19,13 @@ public class RegistrationController {
 
     @PostMapping("/register")
     public ResponseEntity<String> register(@RequestBody User user) {
-        userService.register(user);
-        return ResponseEntity.ok("User registered successfully");
+        try {
+            userService.register(user);
+            return ResponseEntity.ok("User registered successfully");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("Error registering user: " + e.getMessage());
+        }
     }
 
 }
